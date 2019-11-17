d6f94189cdf412b3e96d8d2aae7bf194e205f225
Sasha Haywood <haywood@iu.edu>
2019-11-17 08:22:00 -0500

    added part 3 but still has mistakes

--- HW7.R
+++ HW7.R
@@ -14,9 +14,32 @@ problem = Problem(objective)
 result = solve(problem)
 result$getValue(theta_lasso)
 fitted_theta = result$getValue(theta_lasso)
+fitted_theta = round(fitted_theta, digits = 2)
 
 ## Step 3
-
+## When I try to do this with a subset of data, it doesn't work.  
+subset = data[1:3, 1:3]
+S = cov(data)
+p = dim(data)[2]
+theta_lasso = Variable(p,p)
+lambda_search = 10^(seq(-2, 2, length.out = 40))
+get_theta_hat_lasso = function(lambda) {
+  objective = Minimize(-log_det(theta_lasso) + matrix_trace(S %*% theta_lasso) +
+                         lambda * sum(abs(theta_lasso)))
+  problem = Problem(objective)
+  result = solve(problem)
+  result$getValue(theta_lasso)
+}
+theta_hats = plyr::aaply(lambda_search, 1, get_theta_hat_lasso)
+theta_hats = round(theta_hats, digits = 2)
+colnames(theta_hats) = colnames(data)
+theta_hats = cbind(lambda = lambda_search, theta_hats)      ## something goes wrong here. In her example in 
+## lecture 21, beta_lasso was a vector, but ours is a matrix.  I'm not sure how things need to change to
+## accomodate that.
+theta_melted = reshape2::melt(data.frame(theta_hats), id.vars = "lambda", value.name = "coefficient")
+ggplot(theta_melted) +
+  geom_line(aes(x = lambda, y = coefficient, color = variable, lty = variable)) +
+  scale_x_log10()
 
 ## Step 4
 I = split(data, sample(rep(1:10, 5)))
@@ -54,25 +77,5 @@ plot(overallNegLogLik, fitted_theta)
 
 
 
-# other garbage I might need
-lambda_search = 10^(seq(-2, 2, length.out = 40))
-get_theta_hat_lasso = function(lambda) {
-  objective = Minimize(-log_det(theta_lasso) + matrix_trace(S %*% theta_lasso) +
-                         lambda * sum(theta_lasso))
-  problem = Problem(objective)
-  result = solve(problem)
-  result$getValue(theta_lasso)
-}
-theta_hats = plyr::aaply(lambda_search, 1, get_theta_hat_lasso)
-colnames(theta_hats) = colnames(data$X)
-theta_hats = cbind(lambda = lambda_search, theta_hats)
-round(theta_hats, digits = 2)
 
-
-
-
-
-
-objective = Minimize(-log_det(theta_lasso) + matrix_trace(S %*% theta_lasso))
-constraints = list()
                      
